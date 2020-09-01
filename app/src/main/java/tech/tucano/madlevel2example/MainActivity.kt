35dc46a55b6b8a778ee5646013e5c86afe0b8586
ru1t3rl <luca.ruiters@hva.nl>
2020-09-01 09:12:52 +0100

    Setup the RecyclerView

--- app/src/main/java/tech/tucano/madlevel2example/MainActivity.kt
+++ app/src/main/java/tech/tucano/madlevel2example/MainActivity.kt
@@ -2,10 +2,34 @@ package tech.tucano.madlevel2example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import tech.tucano.madlevel2example.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private val reminders = arrayListOf<Reminder>()
+    private val reminderAdapter = ReminderAdapter(reminders)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
+        initViews()
+    }
+
+    private fun initViews(){
+        binding.btnAddReminder.setOnClickListener {
+            val reminder = binding.etReminder.text.toString()
+            addReminder(reminder)
+        }
+
+        binding.rvReminders.layoutManager = LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
+        binding.rvReminders.adapter = reminderAdapter
+
+        binding.rvReminders.addItemDecoration(DividerItemDecoration(this@MainActivity, DividerItemDecoration.VERTICAL))
     }
 }
---
+++ app/src/main/java/tech/tucano/madlevel2example/Reminder.kt
@@ -0,0 +1,5 @@
+package tech.tucano.madlevel2example
+
+data class Reminder (
+    var reminderText: String
+)
